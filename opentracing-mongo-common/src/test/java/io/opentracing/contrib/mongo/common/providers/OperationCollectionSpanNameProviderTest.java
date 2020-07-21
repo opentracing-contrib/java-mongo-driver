6beb8c09087853d0d64af4f2bf0d5e12268e6cb3
Anatolii Papenko <privettoli@gmail.com>
2020-07-20 23:23:28 -0500

    support buildInfo command in OperationCollectionSpanNameProvider (#19)

--- opentracing-mongo-common/src/test/java/io/opentracing/contrib/mongo/common/providers/OperationCollectionSpanNameProviderTest.java
+++ opentracing-mongo-common/src/test/java/io/opentracing/contrib/mongo/common/providers/OperationCollectionSpanNameProviderTest.java
@@ -13,33 +13,46 @@
  */
 package io.opentracing.contrib.mongo.common.providers;
 
-import static org.junit.Assert.assertEquals;
-
 import com.mongodb.ServerAddress;
 import com.mongodb.connection.ClusterId;
 import com.mongodb.connection.ConnectionDescription;
 import com.mongodb.connection.ServerId;
 import com.mongodb.event.CommandStartedEvent;
 import org.bson.BsonDocument;
+import org.bson.BsonInt32;
 import org.bson.BsonString;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class OperationCollectionSpanNameProviderTest {
 
   private final MongoSpanNameProvider provider = new OperationCollectionSpanNameProvider();
 
-  private final CommandStartedEvent TEST_EVENT = new CommandStartedEvent(1,
-      new ConnectionDescription(new ServerId(new ClusterId(), new ServerAddress())),
-      "database-name", "insert",
-      new BsonDocument().append("insert", new BsonString("collection-name")));
+  private final CommandStartedEvent INSERT_TEST_EVENT = new CommandStartedEvent(1,
+    new ConnectionDescription(new ServerId(new ClusterId(), new ServerAddress())),
+    "database-name", "insert",
+    new BsonDocument().append("insert", new BsonString("collection-name")));
+
+  private final CommandStartedEvent BUILD_INFO_TEST_EVENT = new CommandStartedEvent(44,
+    new ConnectionDescription(new ServerId(new ClusterId(), new ServerAddress())),
+    "database-name", "buildInfo",
+    new BsonDocument()
+      .append("buildInfo", new BsonInt32(1))
+      .append("$db", new BsonString("database-name")));
 
   @Test
   public void testOperationNameExists() {
-    assertEquals("insert collection-name", provider.generateName(TEST_EVENT));
+    assertEquals("insert collection-name", provider.generateName(INSERT_TEST_EVENT));
   }
 
   @Test
   public void testNullOperationName() {
     assertEquals("unknown", provider.generateName(null));
+  }
+
+  @Test
+  public void testOperationNameNotString() {
+    assertEquals("buildInfo", provider.generateName(BUILD_INFO_TEST_EVENT));
   }
 }
